f2ee221c10c9fd85d594525f7e48e55b7396850b
ErickMoraL <erickmora0503@gmail.com>
2024-04-22 22:03:05 -0600

    ab

---
+++ SEMANA5/app/src/main/java/com/example/erickml/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.erickml
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnAccept: Button
+    private lateinit var textview1: TextView
+    private lateinit var nombre: EditText
+    private lateinit var apellido: EditText
+    private lateinit var textView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        /*
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        */
+
+        textView = findViewById(R.id.textView)
+        nombre = findViewById(R.id.nombre)
+        apellido = findViewById(R.id.apellido)
+        apellido.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                textView.text = apellido.text
+            }
+        })
+    }
+}
